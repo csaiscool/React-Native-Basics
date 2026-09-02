5e438d57096be5040ebde4db716149a45f96adb7
csaiscool <lokasathvik@gmail.com>
2026-09-02 10:06:31 -0500

    .

---
+++ lesson67/styles/colors.tsx
@@ -0,0 +1,11 @@
+const colors = {
+    primary: '#cbf6db',
+    secondary: '#ccd5ae',
+    background: '#f5beb4',
+    text: {
+        light: '#fefae0',
+        dark: '#d4a373'
+    }
+}
+
+export default colors;
---
+++ lesson67/styles/defaultStyles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const defaultStyles = () => {
+  return (
+    <View>
+      <Text>defaultStyles</Text>
+    </View>
+  )
+}
+
+export default defaultStyles
+
+const styles = StyleSheet.create({})
